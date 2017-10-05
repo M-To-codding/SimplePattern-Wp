9aae791ecf0b68d3210da9406aa1ca09777627ef
im <=>
2017-10-05 15:35:36 +0300

    Added styles for sidebar and widgets, fixed main page

--- wp-content/themes/simple-project/template-parts/content-front-page.php
+++ wp-content/themes/simple-project/template-parts/content-front-page.php
@@ -26,38 +26,6 @@
             </a>
 
         <?php
-		}
-		if ( 'post' === get_post_type() ) : ?>
-            <div class="entry-meta">
-				<?php simple_project_posted_on(); ?>
-            </div><!-- .entry-meta -->
-			<?php
-		endif; ?>
+		} ?>
     </header><!-- .entry-header -->
-
-    <div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-			/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'simple-project' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'simple-project' ),
-			'after'  => '</div>',
-		) );
-		?>
-    </div><!-- .entry-content -->
-
-    <footer class="entry-footer">
-		<?php simple_project_entry_footer(); ?>
-    </footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
