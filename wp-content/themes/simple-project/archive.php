51e430b9ca76db88bfa5dc07dfd0d9cef7933d23
im <=>
2017-10-08 18:18:03 +0300

    Fixed archive page for displaying posts in selected category

--- wp-content/themes/simple-project/archive.php
+++ wp-content/themes/simple-project/archive.php
@@ -9,43 +9,40 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+    <div id="primary" class="gallery-content">
+        <main class="main-content">
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			if ( have_posts() ) :
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
 
-			endwhile;
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
 
-			the_posts_navigation();
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', get_post_format() );
 
-		else :
+				endwhile;
 
-			get_template_part( 'template-parts/content', 'none' );
+				$args = array( 'screen_reader_text' => ( ' ' ), );
+				the_posts_navigation( $args );
 
-		endif; ?>
+			else :
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				get_template_part( 'template-parts/content', 'none' );
 
+			endif; ?>
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
+    <div class="sidebar-bg">
+		<?php
+		get_sidebar();
+		?>
+    </div>
 <?php
-get_sidebar();
 get_footer();
