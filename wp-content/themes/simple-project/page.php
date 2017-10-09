83ab2c11f7d2661a77c9bc4c9f3c014234bc3748
im <=>
2017-10-09 13:22:47 +0300

    Fixed main page: added custom fields instead static text

--- wp-content/themes/simple-project/page.php
+++ wp-content/themes/simple-project/page.php
@@ -20,28 +20,24 @@ get_header(); ?>
 
                 <section class="container about">
                     <h2 class="heading">
-                        <a href="<?php echo get_page_link(48)?>">
+                        <a href="<?php echo get_page_link( 48 ) ?>">
                             Lorem ipsum
                         </a>
                     </h2>
 
                     <p class="info">
-                        Lorem ipsum dolor sit amet,
-                        consectetur adipisicing elit. Assumenda
-                        cumque cupiditate dicta ex exercitationem
-                        facere fugiat impedit iste laudantium magni
-                        mollitia natus non odio.
+						<?php the_field( 'main-page-about' ); ?>
                     </p>
 
                     <div class="goto">
-                        <a href="<?php echo get_page_link(48)?>">More</a>
+                        <a href="<?php echo get_page_link( 48 ) ?>">More</a>
                     </div>
                 </section>
             </div>
 
             <section class="works">
                 <h2 class="heading">
-                    <a href="<?php echo get_permalink( get_option('page_for_posts')); ?>">
+                    <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">
                         Lorem ipsum
                     </a>
                 </h2>
@@ -52,18 +48,18 @@ get_header(); ?>
 						<?php
 						$the_query = new WP_Query( 'posts_per_page=2' ); ?>
 
-	                    <?php while ($the_query -> have_posts()) : $the_query -> the_post();
+						<?php while ( $the_query->have_posts() ) : $the_query->the_post();
 
 							get_template_part( 'template-parts/content-front-page', get_post_format() );
 
-	                    endwhile;
-	                    wp_reset_postdata();
-	                     ?>
+						endwhile;
+						wp_reset_postdata();
+						?>
 
                     </div>
 
                     <div class="goto">
-                        <a href="<?php echo get_permalink( get_option('page_for_posts')); ?>">More</a>
+                        <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">More</a>
                     </div>
                 </div>
             </section>
